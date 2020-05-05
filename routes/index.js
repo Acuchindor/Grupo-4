4e2568db1bacbd4054de628ed02c1353f1e29d70
Auchindor <cristiangarciacorral@gmail.com>
2020-05-05 20:28:21 +0200

    aa

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,18 @@ router.get('/', function(req, res, next) {
 router.get('/register', function (req, res) {
   res.sendFile(path.join('C:/Users/crist/Desktop/untitled/public/registro.html'));
 });
+router.post('/register', function (req, res) {
+  var email = req.body.email;
+  var reemail = req.body.reemail;
+  var username = req.body.username;
+  var passwd = req.body.passwd;
+  var repasswd = req.body.passwd;
+  // console.log('usuario = '+username+' email ='+ email);
+
+  if (email.toString() == reemail.toString() && passwd.toString() == repasswd.toString()){
+    console.log('usuario = '+username+' email ='+ email);
+  }else{
+    console.log('No funciona');
+  }
+});
 module.exports = router;
