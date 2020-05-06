cb94324f1acb84f84765c402c02bac055aaa3458
Auchindor <cristiangarciacorral@gmail.com>
2020-05-06 19:43:51 +0200

    aa

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var User = require('../lib/User');
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/MONGOL", {
+  "auth": { "authSource": "MONGOL" },
+  "user": "Admin",
+  "pass": "Almi123",
+  "useMongoClient": true
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,10 +25,10 @@ router.post('/register', function (req, res) {
   var passwd = req.body.passwd;
   var repasswd = req.body.passwd;
   if (email.toString() == reemail.toString() && passwd.toString() == repasswd.toString()){
-    var usuario = new User({email: email, username: username, password: passwd})
+    var usuario = new User({email: email, username: username, password: passwd});
     usuario.save(function (err, user) {
       if (err) return console.error(err);
-      console.log(user.username + "guardado");
+      console.log(user.username + " guardado");
     })
   }else{
     console.log('No funciona');
